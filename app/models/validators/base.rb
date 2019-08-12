c0ee00527d68ceea356f46b18296f358bebda37e
Nikita Silivonchik <lstpsche@gmail.com>
2019-08-12 21:53:53 +0300

    LP-100 create reek config file; fix all reek issues

--- app/models/validators/base.rb
+++ app/models/validators/base.rb
@@ -4,16 +4,17 @@ module Validators
   class Base < ActiveModel::Validator
     def validate(record)
       key = record.class.to_s.underscore
+      fields = options[:fields]
 
-      validators[key]&.validate(record)
+      validators[key].validate(record)
     end
 
     private
 
     def validators
       {
-        'ticket' => Validators::TicketsValidator.new(options[:fields]),
-        'item' => Validators::ItemsValidator.new(options[:fields])
+        'ticket' => Validators::TicketsValidator.new(fields),
+        'item' => Validators::ItemsValidator.new(fields)
       }
     end
   end
