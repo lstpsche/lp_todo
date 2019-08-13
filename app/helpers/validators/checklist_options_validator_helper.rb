0968cbb6d7c02040a125695c03df0fd3b5d7a512
Nikita Silivonchik <lstpsche@gmail.com>
2019-08-13 09:51:06 +0300

    LP-74 fix rubocop issues

--- app/helpers/validators/checklist_options_validator_helper.rb
+++ app/helpers/validators/checklist_options_validator_helper.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 module Validators
   module ChecklistOptionsValidatorHelper
-    INVALID_OPTION_VALUE_MSG = "Invalid checklist option value. It should be either checked or not checked."
-    TEXT_NOT_PRESENT_MSG = "Checklist option should have a text. Please, specify one."
-    CHECKLIST_NOT_PRESENT_MSG = "Checklist option should be in Checklist."
+    INVALID_OPTION_VALUE_MSG = 'Invalid checklist option value. It should be either checked or not checked.'
+    TEXT_NOT_PRESENT_MSG = 'Checklist option should have a text. Please, specify one.'
+    CHECKLIST_NOT_PRESENT_MSG = 'Checklist option should be in Checklist.'
 
     def validate_option_checklist_presence
       option.errors[:base] << CHECKLIST_NOT_PRESENT_MSG unless option.checklist
