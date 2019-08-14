bccc503c89fc0642b41d38a56df7c2bcb123c913
Nikita Silivonchik <lstpsche@gmail.com>
2019-08-15 01:11:54 +0300

    Refactor Base validator

--- app/models/validators/base.rb
+++ app/models/validators/base.rb
@@ -2,29 +2,23 @@
 
 module Validators
   class Base < ActiveModel::Validator
+    VALIDATORS = {
+      'checklist' => Validators::Checklists,
+      'checklist_option' => Validators::ChecklistOptions,
+      'checklist_option_due_time' => Validators::ChecklistOptionsDueTime,
+      'due_time' => Validators::DueTime,
+      'folder' => Validators::Folders,
+      'label' => Validators::Labels,
+      'note' => Validators::Notes,
+      'ticket' => Validators::Tickets,
+      'item' => Validators::Items,
+      'workspace' => Validators::Workspaces
+    }.freeze
+
     def validate(record)
       key = record.class.to_s.underscore
 
       validators[key].new(options[:fields]).validate(record)
-    end
-
-    private
-
-    attr_reader :fields
-
-    def validators
-      {
-        'checklist' => Validators::Checklists,
-        'checklist_option' => Validators::ChecklistOptions,
-        'checklist_option_due_time' => Validators::ChecklistOptionsDueTime,
-        'due_time' => Validators::DueTime,
-        'folder' => Validators::Folders,
-        'label' => Validators::Labels,
-        'note' => Validators::Notes,
-        'ticket' => Validators::Tickets,
-        'item' => Validators::Items,
-        'workspace' => Validators::Workspaces
-      }
     end
   end
 end
